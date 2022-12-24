21d2aea4ec823ba42d82480a4f47509e6d12804b
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2022-12-24 12:05:08 -0300

    map em arrays

--- 2_arrays/filter/index.js
+++ 2_arrays/filter/index.js
@@ -12,9 +12,9 @@ const numeros = [5, 65, 1, 3, 12, 23, 34, 11, 6, 8, 43, 17]
 const numerosFiltrados = numeros.filter(valor => valor > 10)
 console.log(numerosFiltrados)
 
-// Retorne as pessoas que tenham um nome com 5 letras ou mais
-// Retorne as pessoas que tenham mais de 25 anos
-// Retorne as pessoas cujo o nome termine com a letra a
+// 1 -> Retorne as pessoas que tenham um nome com 5 letras ou mais
+// 2 -> Retorne as pessoas que tenham mais de 25 anos
+// 3 -> Retorne as pessoas cujo o nome termine com a letra a
 const pessoas = [
     {nome: 'Matheus', idade: 25},
     {nome: 'Amanda', idade: 20},
@@ -22,10 +22,14 @@ const pessoas = [
     {nome: 'Stefany', idade: 26},
     {nome: 'Robson', idade: 36}
 ]
+// 1°
 const pessoasComNomeGrande = pessoas.filter(obj => obj.nome.length >= 5)
-const pessoasComMaisDeVinteeCincoAnos = pessoas.filter(obj => obj.idade > 25)
-const pessoasComNomeTerminadoEmA = pessoas.filter(obj => obj.nome.toLowerCase().endsWith('a'))
-
 console.log(pessoasComNomeGrande)
+
+// 2°
+const pessoasComMaisDeVinteeCincoAnos = pessoas.filter(obj => obj.idade > 25)
 console.log(pessoasComMaisDeVinteeCincoAnos)
+
+// 3°
+const pessoasComNomeTerminadoEmA = pessoas.filter(obj => obj.nome.toLowerCase().endsWith('a'))
 console.log(pessoasComNomeTerminadoEmA)
---
+++ 2_arrays/map/index.js
@@ -0,0 +1,38 @@
+// Dobre os números
+const numeros = [5, 65, 1, 3, 12, 23, 34, 11, 6, 8, 43, 17]
+const numerosEmDobro = numeros.map(valor => valor * 2)
+
+console.log(numerosEmDobro)
+
+// 1 -> Retorne apenas uma string com o nome da pessoa
+// 2 -> Remova apenas a chave "nome" do objeto
+// 3 -> dicione a chave id em cada objeto
+const pessoas = [
+    {nome: 'Matheus', idade: 25},
+    {nome: 'Amanda', idade: 20},
+    {nome: 'Alex', idade: 17},
+    {nome: 'Stefany', idade: 26},
+    {nome: 'Robson', idade: 36}
+]
+// 1°
+const nomePessoa = pessoas.map(pessoa => pessoa.nome)
+console.log(nomePessoa)
+
+// 2°
+const idadePessoa = pessoas.map(pessoa => {
+    const newObj = { ...pessoa }
+    delete newObj.nome
+    return newObj
+})
+console.log(idadePessoa)
+// ou
+const novoArrayIdadePessoa = pessoas.map(pessoa => ({ idade: pessoa.idade }))
+console.log(novoArrayIdadePessoa)
+
+// 3°
+const idPessoa = pessoas.map((pessoa, indice) => {
+    const newObj = { ...pessoa }
+    newObj.id = indice
+    return newObj
+})
+console.log(idPessoa)
