2d2406053de643a28a8075810cc049decc4fbf08
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2022-12-26 21:18:04 -0300

    resolução do exercicio

--- 3_objetos/7_Exercicio/resolucao.js
+++ 3_objetos/7_Exercicio/resolucao.js
@@ -14,3 +14,61 @@ Se o número for maior que 9, consideramos 0.
 11 - (284 % 11) = 2 (Segundo dígito)
 Se o número for maior que 9, consideramos 0.
 */
+
+// let cpf = '705.484.450-52'
+// let cpfLimpo = cpf.replace(/\D+/g, '')
+// let cpfArray = Array.from(cpfLimpo)
+// console.log(cpfArray)
+// const total = cpfArray.reduce((ac, valor) => {
+//     ac += valor
+//     return ac
+// })
+// console.log(total)
+
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function() {
+            return cpfEnviado.replace(/\D+/g, '')
+        }
+    })
+}
+
+ValidaCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undeffined') return false
+    if(this.cpfLimpo.length !== 11) return false
+    if(this.isSequencia()) return false
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2)
+    const digito1 = this.criaDigito(cpfParcial)
+    const digito2 = this.criaDigito(cpfParcial + digito1)
+    
+    const novoCpf = cpfParcial + digito1 + digito2
+    return novoCpf === this.cpfLimpo
+}
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial)
+    let regressivo = cpfArray.length +1
+    const total = cpfArray.reduce((ac, valor) => {
+        ac += (regressivo * Number(valor))
+        regressivo--
+        return ac
+    }, 0)
+
+    const digito = 11 - (total % 11) 
+    return digito > 9 ? '0' : String(digito)
+}
+
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
+    return sequencia === this.cpfLimpo
+}
+
+const cpf = new ValidaCPF('705.484.450-52')
+
+if(cpf.valida()) {
+    console.log('CPF válido')
+} else {
+    console.log('CPF inválido')
+}
